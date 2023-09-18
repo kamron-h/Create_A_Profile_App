4f48aa4fa631a3c6432b55dc8757cead89410bd5
Kamron <kamronthopkins@gmail.com>
2023-09-17 23:19:29 -0400

    Created Second View and Added UI Components

--- app/src/main/java/com/example/create_a_profile_app/MainActivity.java
+++ app/src/main/java/com/example/create_a_profile_app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.create_a_profile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button profile_button = (Button) findViewById(R.id.profile_button);
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/create_a_profile_app/MainActivity2.java
+++ app/src/main/java/com/example/create_a_profile_app/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.create_a_profile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button home_button = (Button) findViewById(R.id.home_button);
+
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
