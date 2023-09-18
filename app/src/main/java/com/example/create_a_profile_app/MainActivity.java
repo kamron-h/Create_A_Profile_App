301f3d2d879b80bc7e25ac71c6fb6f703e00c7a1
Kamron <kamronthopkins@gmail.com>
2023-09-18 04:14:16 -0400

    Pushed Input Data to NewActivity

--- app/src/main/java/com/example/create_a_profile_app/MainActivity.java
+++ app/src/main/java/com/example/create_a_profile_app/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,14 +20,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button profile_button = (Button) findViewById(R.id.profile_button);
+        Button profile_button = findViewById(R.id.profile_button);
+
+        TextInputEditText name_input_widget = findViewById(R.id.name_input);
+        TextInputEditText age_input_widget = findViewById(R.id.age_input);
+        RadioGroup radio_group_widget = findViewById(R.id.radio_group);
+        CheckBox checkbox1_widget = findViewById(R.id.checkbox1);
+        CheckBox checkbox2_widget = findViewById(R.id.checkbox2);
 
         profile_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Get the input data
+                String name = name_input_widget.getText().toString();
+                String age = age_input_widget.getText().toString();
+                int selectedRadioId = radio_group_widget.getCheckedRadioButtonId();
+                boolean isCheckedCheckbox1 = checkbox1_widget.isChecked();
+                boolean isCheckedCheckbox2 = checkbox2_widget.isChecked();
+
+                // Check if the name and age are empty and provide default values if needed
+                if (TextUtils.isEmpty(name)) {
+                    name = "Guest"; // Default value if the name is empty
+                }
+                if (TextUtils.isEmpty(age)) {
+                    age = "11"; // Default value if the age is empty
+                }
+
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+                // Pass the input data as extras to the intent
+                intent.putExtra("name", name);
+                intent.putExtra("age", age);
+                intent.putExtra("selectedRadioId", selectedRadioId);
+                intent.putExtra("isCheckedCheckbox1", isCheckedCheckbox1);
+                intent.putExtra("isCheckedCheckbox2", isCheckedCheckbox2);
+
                 startActivity(intent);
             }
         });
     }
 }
+
--- app/src/main/java/com/example/create_a_profile_app/MainActivity2.java
+++ app/src/main/java/com/example/create_a_profile_app/MainActivity2.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,8 +16,36 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Button home_button = (Button) findViewById(R.id.home_button);
+        // Retrieve data passed from MainActivity
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String age = intent.getStringExtra("age");
+        int selectedRadioId = intent.getIntExtra("selectedRadioId", -1);
+        boolean isCheckedCheckbox1 = intent.getBooleanExtra("isCheckedCheckbox1", false);
+        boolean isCheckedCheckbox2 = intent.getBooleanExtra("isCheckedCheckbox2", false);
 
+        // Display the data (you can use TextViews or any other UI elements)
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        nameTextView.setText("Name: " + name);
+
+        TextView ageTextView = findViewById(R.id.ageTextView);
+        ageTextView.setText("Age: " + age);
+
+//        TextView genderTextView = findViewById(R.id.genderTextView);
+//        RadioButton selectedRadioButton = findViewById(selectedRadioId);
+//        genderTextView.setText("Selected Radio Option: " + selectedRadioButton.getText());
+
+        TextView interestCheckbox = findViewById(R.id.interestCheckboxTextView);
+        String checkboxesText = "Selected Checkboxes: ";
+        if (isCheckedCheckbox1) {
+            checkboxesText += "Interest 1 ";
+        }
+        if (isCheckedCheckbox2) {
+            checkboxesText += "Interest 2 ";
+        }
+        interestCheckbox.setText(checkboxesText);
+
+        Button home_button = findViewById(R.id.home_button);
         home_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
